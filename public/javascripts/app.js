32f102bd30c3989d4ba7f45673eef0b572370dc4
Sam Genduso <sam.genduso@gmail.com>
2015-08-22 18:31:45 -0600

    posts display

---
+++ public/javascripts/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('reddit', ['ngMdIcons']);
+
+app.controller('Posts', function ($scope) {
+  $scope.posts=[
+    {
+      title: 'pups in a row',
+      author: 'puppup',
+      imgUrl: 'http://www.eveshamvet.com/wp-content/uploads/2015/06/Evesham-Veterinary-Clinic-Marlton-NJ-Puppies.jpg',
+      description: 'zomgpuppies'
+    },
+    {
+      title: 'collar pup',
+      author: 'puppup',
+      imgUrl: 'http://media.mydogspace.com.s3.amazonaws.com/wp-content/uploads/2013/08/puppy-500x350.jpg',
+      description: 'i think your collar is too big'
+    },
+    {
+      title: 'sir pup',
+      author: 'puppup',
+      imgUrl: 'http://cdn.sheknows.com/articles/2013/04/Photo_1.jpg',
+      description: 'i say, that is sir pup to you'
+    },
+  ];
+});
